00828b31b13bf0fa2522f0356d42cc37367fc9f5
briandenicola <brian@briandenicola.com>
2024-03-12 21:57:30 -0500

    Added name resolution to script

--- src/wrapper.ps1
+++ src/wrapper.ps1
@@ -1,3 +1,12 @@
-Install-Module -name sqlserver -Confirm:$false -Force
+function Resolve-HostName {
+    param(
+        [string] $hostname
+    )
+
+    $ip = [System.Net.Dns]::GetHostAddresses($hostname) | Select-Object -First 1 -ExpandProperty IPAddressToString
+    return $ip
+}
+
+Write-Host "Resolving hostname...{0}: {1}" -f $ENV:DB_HOST, (Resolve-HostName $ENV:DB_HOST)
 Invoke-Sqlcmd -ConnectionString $ENV:CONN_STR -InputFile "/app/sample.sql"
 Invoke-Sqlcmd -ConnectionString $ENV:CONN_STR -Query "exec sp_columns todos"
