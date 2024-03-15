c6914f925469f3b63b82508012ddc90f3273ab55
briandenicola <brian@briandenicola.com>
2024-03-15 08:50:21 -0500

    Added sqlcmd to devcontainer

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -8,6 +8,12 @@ echo "$(date)    post-create start" >> ~/status
 apt update
 apt install -y jq
 
+# Install sqlcmd
+curl https://packages.microsoft.com/keys/microsoft.asc | sudo tee /etc/apt/trusted.gpg.d/microsoft.asc
+add-apt-repository "$(wget -qO- https://packages.microsoft.com/config/ubuntu/20.04/prod.list)"
+apt-get update
+apt-get install -y sqlcmd
+
 #Install Taskdev
 sh -c "$(curl --location https://taskfile.dev/install.sh)" -- -d -b ~/.local/bin
 echo 'export PATH=$PATH:~/.local/bin' >> ~/.bashrc
@@ -16,6 +22,7 @@ echo 'export PATH=$PATH:~/.local/bin' >> ~/.zshrc
 #Install az extensions
 sudo az aks install-cli -y
 sudo az extension add --name application-insights -y
-sudo az extension add --name aks-preview -y
+sudo az extension add --name log-analytics -y
+sudo az extension add --name containerapp -y
 
 echo "$(date)    post-create complete" >> ~/status
