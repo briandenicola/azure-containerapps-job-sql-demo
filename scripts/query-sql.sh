5ad135ec39553c45fe55037bf67e5fe660c08a10
briandenicola <brian@briandenicola.com>
2024-03-19 10:53:37 -0500

    Query script

---
+++ scripts/query-sql.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+while (( "$#" )); do
+  case "$1" in
+    -h|--help)
+      echo "Usage: ./set-sql.sh 
+      "
+      exit 0
+      ;;
+    --) 
+      shift
+      break
+      ;;
+    -*|--*=) 
+      echo "Error: Unsupported flag $1" >&2
+      exit 1
+      ;;
+  esac
+done
+
+source $(dirname $0)/setup-env.sh
+
+SQL_AUTH_METHOD=ActiveDirectoryManagedIdentity
+SQL_DB_NAME=todo 
+SQL_SERVER_FQDN=`echo $CON_STR | awk -F[\;:] '{ print $2 }'`
+AZURE_USERNAME=`echo $CON_STR | awk -F[\;=] '{ print $6 }'`
+
+echo "Simple Query. . ."
+sqlcmd --authentication-method=${SQL_AUTH_METHOD} -U ${AZURE_USERNAME} -S ${SQL_SERVER_FQDN} -d ${SQL_DB_NAME} --query "SELECT * FROM todos;"
